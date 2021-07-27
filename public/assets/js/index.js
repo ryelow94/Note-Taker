2ac4efea2f8d938fc18041da1aaf6cb6228d1917
Ryan Logan <ryan_plogan@yahoo.com>
2021-07-27 15:31:09 -0700

    fixed port issue

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -45,9 +45,9 @@ const saveNote = (note) =>
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
     method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    // headers: {
+    //   'Content-Type': 'application/json',
+    // },
   });
 
 const renderActiveNote = () => {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express') 
 const app = express() 
-const PORT = 3000 
+const PORT = process.env.PORT || 3000
 const path = require('path') 
 let notes = require("./db/db.json") 
 console.log(notes) 
